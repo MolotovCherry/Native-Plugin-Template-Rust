3ac5d36f77a4faaaf7e93329968ef62641471a16
Cherry <13651622+MolotovCherry@users.noreply.github.com>
2024-07-01 07:55:53 -0700

    Use thread for loading

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@ mod paths;
 mod popup;
 
 use std::ffi::c_void;
+use std::thread;
 
 use eyre::{Context, Error};
 use libmem::*;
@@ -38,43 +39,48 @@ extern "C-unwind" fn DllMain(
     #[allow(clippy::single_match)]
     match fdw_reason {
         DLL_PROCESS_ATTACH => {
-            // Wait for debugger if in debug mode
-            if cfg!(debug_assertions) {
-                let is_debugger_present = || unsafe { IsDebuggerPresent().as_bool() };
-
-                while !is_debugger_present() {
-                    // 60hz polling
-                    std::thread::sleep(std::time::Duration::from_millis(16));
-                }
-            }
-
-            // Set up a custom panic hook so we can log all panics to logfile
-            panic::set_hook();
-
-            // Note: While it's technically safe to panic across FFI with C-unwind ABI, I STRONGLY recommend to
-            // catch and handle ALL panics. If you don't, you could crash the game by accident!
-            //
-            // catch_unwind returns a Result with the panic info, but we actually don't need it, because
-            // we set a panic_hook up at top which will log all panics to the logfile.
-            // if for any reason we can't actually log the panic, we *could* popup a
-            // messagebox instead (for debugging use only of course)
-            let result = std::panic::catch_unwind(|| {
-                // set up our actual log file handling
+            // you should be doing stuff in a thread, cause what you can do in DllMain is very restricted.
+            // the best thing would be if the loader does a callback on an exported Init fn, but unfortunately
+            // many loads don't do this.
+            thread::spawn(move || {
+                // Wait for debugger if in debug mode
                 if cfg!(debug_assertions) {
-                    debug_console(LevelFilter::Trace, "Native Plugin Template Debug Console")?;
-                } else {
-                    setup_logging(module).context("Failed to setup logging")?;
+                    let is_debugger_present = || unsafe { IsDebuggerPresent().as_bool() };
+
+                    while !is_debugger_present() {
+                        // 60hz polling
+                        std::thread::sleep(std::time::Duration::from_millis(16));
+                    }
                 }
 
-                entry(module);
+                // Set up a custom panic hook so we can log all panics to logfile
+                panic::set_hook();
 
-                Ok::<_, Error>(())
+                // Note: While it's technically safe to panic across FFI with C-unwind ABI, I STRONGLY recommend to
+                // catch and handle ALL panics. If you don't, you could crash the game by accident!
+                //
+                // catch_unwind returns a Result with the panic info, but we actually don't need it, because
+                // we set a panic_hook up at top which will log all panics to the logfile.
+                // if for any reason we can't actually log the panic, we *could* popup a
+                // messagebox instead (for debugging use only of course)
+                let result = std::panic::catch_unwind(|| {
+                    // set up our actual log file handling
+                    if cfg!(debug_assertions) {
+                        debug_console(LevelFilter::Trace, "Native Plugin Template Debug Console")?;
+                    } else {
+                        setup_logging(module).context("Failed to setup logging")?;
+                    }
+
+                    entry(module);
+
+                    Ok::<_, Error>(())
+                });
+
+                // If there was no panic, but error was bubbled up, then log the error
+                if let Ok(Err(e)) = result {
+                    error!("{e}");
+                }
             });
-
-            // If there was no panic, but error was bubbled up, then log the error
-            if let Ok(Err(e)) = result {
-                error!("{e}");
-            }
         }
 
         _ => (),
